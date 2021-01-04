860dc0560a3f87b946eb50b11199afd0467f9ae4
StrongerL <871682558@qq.com>
2021-01-04 10:21:49 +0800

    74、商品服务-API-属性分组-分组修改&级联选择器回显

--- gulimall-product/src/main/java/com/atguigu/gulimall/product/controller/AttrGroupController.java
+++ gulimall-product/src/main/java/com/atguigu/gulimall/product/controller/AttrGroupController.java
@@ -4,6 +4,7 @@ import java.util.Arrays;
 import java.util.Map;
 
 // import org.apache.shiro.authz.annotation.RequiresPermissions;
+import com.atguigu.gulimall.product.service.CategoryService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -31,6 +32,9 @@ public class AttrGroupController {
     @Autowired
     private AttrGroupService attrGroupService;
 
+    @Autowired
+    CategoryService categoryService;
+
     /**
      * 列表
      */
@@ -53,6 +57,11 @@ public class AttrGroupController {
     // @RequiresPermissions("product:attrgroup:info")
     public R info(@PathVariable("attrGroupId") Long attrGroupId){
 		AttrGroupEntity attrGroup = attrGroupService.getById(attrGroupId);
+
+        Long catelogId = attrGroup.getCatelogId();
+        Long[] path = categoryService.findCatelogPath(catelogId);
+
+        attrGroup.setCatelogPath(path);
 
         return R.ok().put("attrGroup", attrGroup);
     }
