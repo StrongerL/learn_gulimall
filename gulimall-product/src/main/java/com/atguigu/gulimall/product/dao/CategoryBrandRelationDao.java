e273518b2404484e25a453e4014f5c507de830d2
StrongerL <871682558@qq.com>
2021-01-05 19:27:34 +0800

    75、商品服务-API-品牌管理-品牌分类关联与级联更新

--- gulimall-product/src/main/java/com/atguigu/gulimall/product/dao/CategoryBrandRelationDao.java
+++ gulimall-product/src/main/java/com/atguigu/gulimall/product/dao/CategoryBrandRelationDao.java
@@ -3,15 +3,19 @@ package com.atguigu.gulimall.product.dao;
 import com.atguigu.gulimall.product.entity.CategoryBrandRelationEntity;
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
 
 /**
  * 品牌分类关联
- * 
+ *
  * @author stronger
  * @email 123456@gmail.com
  * @date 2020-12-11 15:50:37
  */
 @Mapper
 public interface CategoryBrandRelationDao extends BaseMapper<CategoryBrandRelationEntity> {
-	
+
+    //    void updateCategory(Long catId, String name);
+    void updateCategory(@Param("catId") Long catId, @Param("name") String name);
+
 }
