10609a4b22249daeb70024dbac34dd2d0e456bee
StrongerL <871682558@qq.com>
2022-07-18 23:15:24 +0800

    【151 - 160】添加缓存

--- gulimall-product/src/main/java/com/atguigu/gulimall/product/service/impl/CategoryServiceImpl.java
+++ gulimall-product/src/main/java/com/atguigu/gulimall/product/service/impl/CategoryServiceImpl.java
@@ -1,14 +1,16 @@
 package com.atguigu.gulimall.product.service.impl;
 
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.TypeReference;
 import com.atguigu.gulimall.product.service.CategoryBrandRelationService;
 import com.atguigu.gulimall.product.vo.Catelog2Vo;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.data.redis.core.script.DefaultRedisScript;
 import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
+import java.util.concurrent.TimeUnit;
 import java.util.stream.Collectors;
 
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
@@ -28,6 +30,10 @@ public class CategoryServiceImpl extends ServiceImpl<CategoryDao, CategoryEntity
 
     @Autowired
     CategoryBrandRelationService categoryBrandRelationService;
+
+    @Autowired
+    StringRedisTemplate stringRedisTemplate;
+
 
     @Override
     public PageUtils queryPage(Map<String, Object> params) {
@@ -91,8 +97,45 @@ public class CategoryServiceImpl extends ServiceImpl<CategoryDao, CategoryEntity
         return categoryEntities;
     }
 
+
+    // 为了代码看起来清晰，异常一律抛出
+    public Map<String, List<Catelog2Vo>> getCatalogJsonFromDbWithRedisLock() throws InterruptedException {
+
+        Map<String, List<Catelog2Vo>> ans = null;
+        String catalogJson = stringRedisTemplate.opsForValue().get("catalogJson");
+        if (catalogJson == null) {
+            // 加分布式锁
+            String uuid = UUID.randomUUID().toString();
+            Boolean lock = stringRedisTemplate.opsForValue().setIfAbsent("lock", uuid, 300, TimeUnit.SECONDS);
+            if (Boolean.TRUE.equals(lock)) {
+                // 双重判断
+                catalogJson = stringRedisTemplate.opsForValue().get("catalogJson");
+                if (catalogJson == null) {
+                    Map<String, List<Catelog2Vo>> dataFromDb = getCatalogJsonFromDB();
+                    stringRedisTemplate.opsForValue().set("catalogJson", JSON.toJSONString(dataFromDb), 1, TimeUnit.DAYS);
+                    ans = dataFromDb;
+                    // 删除锁
+                    String script = "if redis.call('get', KEYS[1]) == ARGV[1] then return redis.call('del', KEYS[1]) else return 0 end";
+                    stringRedisTemplate.execute(new DefaultRedisScript<>(script, Long.class), Collections.singletonList("lock"), uuid);
+                }
+            } else {
+                TimeUnit.MILLISECONDS.sleep(100);
+                return getCatalogJsonFromDbWithRedisLock(); // 自旋
+            }
+        } else {
+            ans = JSON.parseObject(catalogJson, new TypeReference<Map<String, List<Catelog2Vo>>>() {});
+        }
+        return ans;
+    }
+
+
     @Override
     public Map<String, List<Catelog2Vo>> getCatalogJson() {
+        return getCatalogJsonFromDB();
+    }
+
+
+    private Map<String, List<Catelog2Vo>> getCatalogJsonFromDB() {
         System.out.println("查询了数据库");
 
         //将数据库的多次查询变为一次
@@ -135,6 +178,7 @@ public class CategoryServiceImpl extends ServiceImpl<CategoryDao, CategoryEntity
 
         return parentCid;
     }
+
 
     private List<CategoryEntity> getParent_cid(List<CategoryEntity> selectList, Long parentCid) {
         List<CategoryEntity> categoryEntities = selectList.stream().
--- gulimall-product/src/test/java/com/atguigu/gulimall/product/GulimallProductApplicationTests.java
+++ gulimall-product/src/test/java/com/atguigu/gulimall/product/GulimallProductApplicationTests.java
@@ -3,7 +3,11 @@ package com.atguigu.gulimall.product;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.redisson.api.RedissonClient;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.data.redis.core.ValueOperations;
 import org.springframework.test.context.junit4.SpringRunner;
 
 
@@ -11,7 +15,30 @@ import org.springframework.test.context.junit4.SpringRunner;
 @SpringBootTest
 public class GulimallProductApplicationTests {
 
+    @Autowired
+    StringRedisTemplate stringRedisTemplate;
+
+    @Autowired
+    RedissonClient redissonClient;
+
+
     @Test
     public void contextLoads(){ }
+
+    @Test
+    public void testRedis(){
+
+        ValueOperations<String, String> ops = stringRedisTemplate.opsForValue();
+        ops.set("hello", "world");
+        System.out.println(ops.get("hello"));
+    }
+
+    @Test
+    public void testRedisson(){
+        System.out.println(redissonClient);
+    }
+
+
+
 
 }
