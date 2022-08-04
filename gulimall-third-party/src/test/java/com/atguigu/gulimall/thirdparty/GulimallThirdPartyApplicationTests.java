4dcfe6caf7a403e9d1cc80d2d48c6557fca3afcc
StrongerL <871682558@qq.com>
2022-08-04 23:33:06 +0800

    【211 - 213】认证服务搭建、短信验证码

--- gulimall-third-party/src/test/java/com/atguigu/gulimall/thirdparty/GulimallThirdPartyApplicationTests.java
+++ gulimall-third-party/src/test/java/com/atguigu/gulimall/thirdparty/GulimallThirdPartyApplicationTests.java
@@ -1,13 +1,23 @@
 package com.atguigu.gulimall.thirdparty;
 
+import com.atguigu.gulimall.thirdparty.component.SmsComponent;
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
+import javax.annotation.Resource;
+
+@RunWith(SpringRunner.class)
 @SpringBootTest
-class GulimallThirdPartyApplicationTests {
+public class GulimallThirdPartyApplicationTests {
+
+    @Resource
+    private SmsComponent smsComponent;
 
     @Test
-    void contextLoads() {
+    public void sendSmsCode() {
+        smsComponent.sendCode("17307485051", "134531");
     }
 
 }
