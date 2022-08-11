f52165381b07c77592b075bb6b98301ad82984fa
StrongerL <871682558@qq.com>
2022-08-11 23:47:43 +0800

    【247 - 261】消息队列

--- gulimall-cart/src/main/java/com/atguigu/gulimall/cart/interceptor/GulimallWebConfig.java
+++ gulimall-cart/src/main/java/com/atguigu/gulimall/cart/interceptor/GulimallWebConfig.java
@@ -4,12 +4,6 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-/**
- * @Description:
- * @Created: with IntelliJ IDEA.
- * @author: 夏沫止水
- * @createTime: 2020-06-30 17:57
- **/
 
 @Configuration
 public class GulimallWebConfig implements WebMvcConfigurer {
